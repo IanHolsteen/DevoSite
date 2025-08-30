98cec0e8dfd2513262d52b6cce20bd0261c835ca
IanHolsteen <ianholsteen@gmail.com>
2025-08-29 20:35:25 -0400

    help build build;

--- next.config.js
+++ next.config.js
@@ -1,35 +1,47 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+ /** @type {import('next').NextConfig} */
 
-}
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*'
-      }
-    ]
-  },
-
-  // distDir: 'build',
+  output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'tailwindui.com',
-        port: '',
-        pathname: '/img/logos/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.github.com',
-        port: '',
-        pathname: '/markgowen/capstone_portfolio/**',
-      },
-    ]
-  }
+  },
+  basePath: isProd ? '/your-repo-name' : '',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
 }
+
+// const nextConfig = {
+//   reactStrictMode: true,
+
+// }
+
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://localhost:3000/api/:path*'
+//       }
+//     ]
+//   },
+
+//   // distDir: 'build',
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'tailwindui.com',
+//         port: '',
+//         pathname: '/img/logos/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'www.github.com',
+//         port: '',
+//         pathname: '/markgowen/capstone_portfolio/**',
+//       },
+//     ]
+//   }
+// }
